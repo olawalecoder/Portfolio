5b10a938f788bc70ee4079a2e19f878a44ead852
Olawale <bolawale81@gmail.com>
2021-12-16 16:23:11 +0100

    Changed typo error in form validation

--- Js/form.js
+++ Js/form.js
@@ -5,11 +5,11 @@ form.addEventListener('submit', (e) => {
   const message = document.getElementById('message');
   const pattern = /^[^]+@[^]+\.[a-z]{2,3}$/;
   if (email === email.toLowerCase() && email.match(pattern)) {
-    message.innerHTML = 'Your Email Address is Valid. Form is sent.';
+    message.innerHTML = 'Your Email Address is Valid. <br> Form was sent.';
     message.style.color = '#F55800';
     form.submit();
   } else {
-    message.innerHTML = 'Please write your email in lowercase!. The form was not sent';
+    message.innerHTML = 'Please write your email in lowercase!. <br> The form was not sent';
     message.style.color = '#ff0000';
     e.preventDefault();
   }
