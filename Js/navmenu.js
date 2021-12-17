f482360697e8bd9f013238ce8a1997c74d93ef26
Olawale <bolawale81@gmail.com>
2021-12-17 16:14:23 +0100

    Worked on project pop up

--- Js/navmenu.js
+++ Js/navmenu.js
@@ -9,15 +9,3 @@ openMenu.addEventListener('click', () => {
 closeMenu.forEach((element) => {
   element.addEventListener('click', () => slideMenu.classList.remove('appear'));
 });
-
-const openPop = document.querySelectorAll('.open-button');
-const closePop = document.querySelector('.title-x');
-const popUp = document.querySelector('.pop-container');
-
-openPop.forEach((element) => {
-  element.addEventListener('click', () => popUp.classList.add('pop'));
-});
-
-closePop.addEventListener('click', () => {
-  popUp.classList.remove('title');
-});
--- Js/popwindow.js
+++ Js/popwindow.js
@@ -76,6 +76,7 @@ for (let i = 0; i < myProjects.length; i++) {
     const project = myProjects[i];
     projectSection.innerHTML += `
       
+
       <div class="project-template">
         <div id=${project.id} class='proj2'> 
           <h3>${project.title}</h3>
@@ -88,53 +89,53 @@ for (let i = 0; i < myProjects.length; i++) {
           <button id='${project.id}' class="Proj-button2 open-button">See Project</button>
         </div>
       </div>
+    
       
       `
 }
 
-// const openPop = document.querySelectorAll('.open-button')
-// const popUp = document.querySelector('.pop-container')
+const openPop = document.querySelectorAll('.open-button')
+const popUp = document.querySelector('.pop-container')
 
-// openPop.forEach(element => {
-//     element.addEventListener('click', () => {
-//         const id = Number(element.id)
-//         for (let i = 0; i < myProjects.length; i++) {
-//             const project = myProjects[i]
-//             if (id === project.id) {
-//                 popUp.innerHTML = `
+openPop.forEach(element => {
+    element.addEventListener('click', () => {
+        const id = Number(element.id)
+        for (let i = 0; i < myProjects.length; i++) {
+            const project = myProjects[i]
+            if (id === project.id) {
+                popUp.innerHTML = `
           
-//         <div class="pop-container">
-//           <div class="title">
-//             <h3 class="title-short">${project.title} </h3>
-//             <h3 class="title-long">${project.titlelong}/h3>
-//             <span class="title-x">&times;</span>
-//           </div>
-//           <ul class="tools">
-//           <li>${project.technologies[0]}</li>
-//           <li>${project.technologies[1]}</li>
-//           <li>${project.technologies[2]}</li>
-//           </ul>
-//           <div class="pop-flex">
-//               <img class="pop-img" src="${project.img}" alt="a visual for project">
-//             <div class="pop-txt">
-//               <p>${project.description}</p>
-//               <div class='pop-button'>
-//                 <button class="Proj-button">See Source <i class="fas fa-external-link-alt"></i></button>
-//                 <button class="Proj-button">See Live <i class="fab fa-github-square"></i></a></li></button>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       `
-//             }
-//         }
+        <div class="pop-container">
+          <div class="title">
+            <h3 class="title-short">${project.title}</h3>
+            <span class="title-x">&times;</span>
+          </div>
+          <ul class="tools">
+          <li>${project.technologies[0]}</li>
+          <li>${project.technologies[1]}</li>
+          <li>${project.technologies[2]}</li>
+          </ul>
+          <div class="pop-flex">
+              <img class="pop-img" src="${project.img}" alt="a visual for project">
+            <div class="pop-txt">
+              <p>${project.description}</p>
+              <div class='pop-button'>
+                <button class="Proj-button">See Source <i class="fas fa-external-link-alt"></i></button>
+                <button class="Proj-button">See Live <i class="fab fa-github-square"></i></a></li></button>
+              </div>
+            </div>
+          </div>
+        </div>
+      `
+            }
+        }
 
-//         popUp.classList.add('pop')
+        popUp.classList.add('pop')
 
-//         const closePop = document.querySelector('.title-x')
-//         closePop.addEventListener('click', () => {
-//             popUp.classList.remove('pop')
-//             console.log('closePop:', closePop)
-//         })
-//     })
-// })
+        const closePop = document.querySelector('.title-x')
+        closePop.addEventListener('click', () => {
+            popUp.classList.remove('pop')
+            console.log('closePop:', closePop)
+        })
+    })
+})
